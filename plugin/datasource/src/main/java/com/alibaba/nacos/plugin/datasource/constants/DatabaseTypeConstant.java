8784efd2a765ba49c6e71fdb1471c1f6e6a0ce64
xiaoyong <56395147+xiaoyonghaoe@users.noreply.github.com>
2023-05-26 13:45:03 +0800

    添加damang数据库插件 (#24)
    * 添加damang插件
    
    * dameng适配mysql的分页
    
    * 添加文档以及注意事项
    
    * 修改错误文字
    
    * 添加表结构相关注意事项
    
    * 添加错误码映射关系
    
    * 适配2.3.0
    
    * 删除脏的语句
    
    * 添加注释
    
    * 添加注释
    
    ---------
    
    Co-authored-by: asdc <asdc>

--- plugin/datasource/src/main/java/com/alibaba/nacos/plugin/datasource/constants/DatabaseTypeConstant.java
+++ plugin/datasource/src/main/java/com/alibaba/nacos/plugin/datasource/constants/DatabaseTypeConstant.java
@@ -25,8 +25,10 @@ public class DatabaseTypeConstant {
 
     public static final String POSTGRESQL = "postgresql";
 
+
     public static final String MYSQL = "mysql";
 
     public static final String ORACLE = "oracle";
-
+  
+    public static final String DM = "dm";
 }
